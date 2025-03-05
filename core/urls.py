0ea4e92c8e401a841ab6fa5ae91dc5c28de67e04
pranjalshukla23 <pranjalshukla23@gmail.com>
2025-03-05 05:31:52 +0100

    added an API to get list of all products

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import add_product, add_to_cart
+from .views import add_product, add_to_cart, get_all_products
 
 urlpatterns = [
+    path('products/', get_all_products, name='get_all_products'),
     path('add-product/', add_product, name="add-product"),
     path('add-to-cart/', add_to_cart, name='add-to-cart'),
 ]
